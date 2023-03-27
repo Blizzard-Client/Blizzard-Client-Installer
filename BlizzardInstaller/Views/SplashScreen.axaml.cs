004f2c66e08cbd8dbc3e2646323470a1984e9241
Facilitated <47868851+FacilitatedIsTaken@users.noreply.github.com>
2023-03-27 14:30:29 -0300

    1

---
+++ BlizzardInstaller/Views/SplashScreen.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BlizzardInstaller.Views;
+
+public partial class SplashScreen : UserControl
+{
+    public SplashScreen()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
