89eac61ceacdc93dceb60fb8e0cb80b1d9489469
Facilitated <47868851+FacilitatedIsTaken@users.noreply.github.com>
2023-03-28 13:29:38 -0300

    1

---
+++ BlizzardInstaller/ViewModels/FirstViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ReactiveUI;
+using Splat;
+
+namespace BlizzardInstaller.ViewModels;
+
+public class FirstViewModel : ReactiveObject, IRoutableViewModel
+{
+    public string UrlPathSegment => "first";
+    public IScreen HostScreen { get; }
+
+    public FirstViewModel(IScreen screen = null)
+    {
+        HostScreen = screen ?? Locator.Current.GetService<IScreen>();
+    }
+}
--- BlizzardInstaller/Views/Introduction.axaml.cs
+++ BlizzardInstaller/Views/Introduction.axaml.cs
@@ -1,21 +1,20 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using BlizzardInstaller.ViewModels;
+using ReactiveUI;
 
 namespace BlizzardInstaller.Views;
 
-public partial class Introduction : Window
+public partial class Introduction : ReactiveUserControl<FirstViewModel>
 {
     public Introduction()
     {
         InitializeComponent();
-#if DEBUG
-        this.AttachDevTools();
-#endif
-    }
-
-    private void InitializeComponent()
-    {
-        AvaloniaXamlLoader.Load(this);
+        this.WhenActivated(disposables =>
+        {
+            this.OneWayBind(ViewModel, x => x.UrlPathSegment, x => x.But)
+        });
     }
 }
